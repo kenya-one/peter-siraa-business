f32bd554db7ad999f1f988f5a85934048c560ba4
kenya-one <lilyb8466@gmail.com>
2026-07-24 11:08:43 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 // After deploying the Google Apps Script as a Web App, paste its URL here.
 // It looks like: https://script.google.com/macros/s/XXXXXXXX/exec
-const APPS_SCRIPT_URL = "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw-XWMbWYXRZcjLEamg_rV74rrfneUn01NHucw4uUj9nM89Wyy6qLuh-A1rU2saGps/exec";
 
 // Business contact details shown in the footer / fallback links
 const BUSINESS = {
   name: "Peter Siraa Land",
-  whatsapp: "254700000000",       // international format, no + or spaces
+  whatsapp: "254792144009",       // international format, no + or spaces
   email: "siraapeter37@gmail.com",
-  facebook: "https://facebook.com/",
-  instagram: "https://instagram.com/"
+  facebook: "https://facebook.com/junioursiraa",
+  instagram: "https://instagram.com/junioursiraa"
 };
